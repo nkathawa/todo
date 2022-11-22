2edf1d6bc59995ab515e5fd1f4149999733b37b3
Navin Kathawa <nkathawa@nutshell.com>
2022-11-22 13:09:32 -0500

    cleanup

--- client/todo/src/App.js
+++ client/todo/src/App.js
@@ -10,14 +10,12 @@ const App = () => {
   const [id, setId] = React.useState(0);
   const [isCreating, setIsCreating] = React.useState(false);
 
-  const handleSaveAllClick = () => {
-    console.log('hi');
+  function handleSaveAllClick() {
   }
 
-  const handleSaveClick = () => {
+  function handleSaveClick() {
     const text = document.getElementById("newTask").value;
     setId(id => id + 1);
-    console.log(id);
     setList(list => [...list, 
       <div key={id}>
         {text}
@@ -27,49 +25,51 @@ const App = () => {
     setButtonPopup(false);
   }
 
-  const handleSaveChangesClick = () => {
-    const text = document.getElementById("editPopup").value;
-    // setId(id => id + 1);
-    // console.log(id);
-    setList(list => [...list, 
-      <div key={id}>
-        {text}
-      </div>
-    ]);
-    setIsCreating(isCreating => false);
+  function handleSaveChangesClick() {
+    handleSaveClick();
     setSaveChangesButtonPopup(false);
   }
 
-  const handleAddClick = () => {
+  function handleAddClick() {
     if(isCreating) {
-      console.log('creating');
       return;
     }
     setIsCreating(isCreating => true);
-    // setId(id => id + 1);
-    // setList(list => [...list, 
-    //   <div key={id}>
-    //     <textarea id="newTask"></textarea>
-    //     <button onClick={handleSaveClick}>Save</button>
-    //   </div>
-    // ]);
     setButtonPopup(true);
   };
 
-  const handleEditClick = () => {
-    console.log('hi');
+  function handleEditClick() {
     setSaveChangesButtonPopup(true);
   };
 
+  function EditPopup(props) {
+    return (props.trigger) ? (
+      <div className='popup'>
+        <textarea id="newTask"></textarea>
+        <button onClick={props.save}>Save Changes</button>
+      </div>
+    ) : "";
+  }
+  
+  function Popup(props) {
+    return (props.trigger) ? (
+      <div className='popup'>
+        <textarea id="newTask"></textarea>
+        <button onClick={props.save}>Save</button>
+      </div>
+    ) : "";
+  }
+
   return (
     <div>
-      <header><h1>To Do</h1></header>
       <main>
+        <div className='mainHeader'>
+          <header><h1>To Do</h1></header>
+        </div>
         <div className='topButtons'>
           <button onClick={handleAddClick}>Add item</button>
           <button onClick={handleSaveAllClick}>Save All</button>
         </div>
-        <br></br>
         <div className='list'>
           {list.map((item) => {
             const handleRemoveClick = () => {
@@ -78,7 +78,7 @@ const App = () => {
             return (
               <div key={item} style={{ display: 'flex', border: '1px solid lightgray' }}>
                 {item}
-                <div style={{ flex: 1 }} /> {/* <-------------------- spacer element */}
+                <div style={{ flex: 1 }} /> {}
                 <button onClick={handleEditClick}>Edit</button>
                 <button onClick={handleRemoveClick}>x</button>
               </div>
@@ -86,39 +86,21 @@ const App = () => {
           })}
         </div>
       </main>
-      <Popup trigger={buttonPopup} setTrigger={setButtonPopup} save={handleSaveClick}>
+      <Popup 
+          trigger={buttonPopup} 
+          setTrigger={setButtonPopup} 
+          save={handleSaveClick}>
         <h3>My popup</h3>
       </Popup>
-      <EditPopup id="editPopup" trigger={saveChangesButtonPopup} setTrigger={setSaveChangesButtonPopup} save={handleSaveChangesClick}>
+      <EditPopup id="editPopup" 
+          trigger={saveChangesButtonPopup} 
+          setTrigger={setSaveChangesButtonPopup} 
+          save={handleSaveChangesClick}>
         <h3>My popup</h3>
       </EditPopup>
     </div>
   );
 };
-
-function EditPopup(props) {
-  return (props.trigger) ? (
-    <div className='popup'>
-      <textarea id="newTask"></textarea>
-      <button onClick={props.save}>Save Changes</button>
-    </div>
-  ) : "";
-}
-
-function Popup(props) {
-  return (props.trigger) ? (
-    <div className='popup'>
-      <textarea id="newTask"></textarea>
-      <button onClick={props.save}>Save</button>
-    </div>
-    // <div className='popup'>
-    //   <div className='popup-inner'>
-    //     <button className='close-btn' onClick={() => props.setTrigger(false)}>close</button>
-    //     { props.children }
-    //   </div>
-    // </div>
-  ) : "";
-}
 
 const root = document.querySelector('#root');
 ReactDOM.render(<App />, root);
